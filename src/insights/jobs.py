f4ac1eb70462a44aeb258a89f10b316c53bab9e6
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-05-04 17:43:06 -0300

    req 6 - filter_by_job_type - done

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -64,4 +64,11 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    # all_jobs = read(__path__)
+    return [
+            job
+            for job in jobs
+            if job["job_type"] == job_type
+        ]
+
