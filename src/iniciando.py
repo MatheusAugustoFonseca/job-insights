5b7490901889d526e745995cc77281402c533718
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-05-08 11:04:34 -0300

    feat req10 - adding test to count_ocurrences - done

--- src/iniciando.py
+++
@@ -1,3 +0,0 @@
-# iniciando o projeto
-print("hello world")
-lista = [1, 2, 3]
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
 # from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "agriculture") == 24
+    assert count_ocurrences("data/jobs.csv", "backend") == 55
+    assert count_ocurrences("data/jobs.csv", "back-end") == 20
+    assert count_ocurrences("data/jobs.csv", "back end") == 8
